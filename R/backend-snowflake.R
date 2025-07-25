788ddd7928c9f9edc0967d1ce7c11311e9f5938c
Eero Saarialho <esaarialho@elan-cap.com>
2025-07-25 13:42:24 +0000

    testi

--- R/backend-snowflake.R
+++ R/backend-snowflake.R
@@ -310,7 +310,7 @@ sql_translation.Snowflake <- function(con) {
           glue_sql2(
             sql_current_con(),
             glue(
-              "TO_NUMBER(MAX_BY({x}, {y})[{n} - 1.0])"
+              "TO_NUMBER(MAX_BY({x}, {y})[{n} - 1])"
             )
           )
         }
